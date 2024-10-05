653902d640ba736be2ccfbcba84a8c57e91a4294
dvh1 <devhumanbeing@gmail.com>
2024-10-05 23:26:02 +0900

    Initial commit

---
+++ src/main/java/com/example/sb3jv17gjt/Sb3jv17gjtApplication.java
@@ -0,0 +1,13 @@
+package com.example.sb3jv17gjt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb3jv17gjtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sb3jv17gjtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sb3jv17gjt/Sb3jv17gjtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sb3jv17gjt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb3jv17gjtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
